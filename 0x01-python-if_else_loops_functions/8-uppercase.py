ddbcd4f30ac0214bcdd7b38c83c3256eebf55795
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2023-01-03 22:55:14 +0300

    Signed-off-by: MarkNWiiam <nkugwamarkwiliam@gmail.com>

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in str:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            i = chr(ord(i) - 32)
-    print("{:s}".format(i), end="")
+for ch in str:
+        # Convert the character to its ASCII value
+        ascii_val = ord(ch)
+        # If the ASCII value is within the range of lowercase letters, subtract 32 to get the uppercase equivalent
+        if ascii_val >= 97 and ascii_val <= 122:
+            ascii_val -= 32
+        # Convert the ASCII value back to a character and print it
+        print(chr(ascii_val), end="")
+    # Print a new line after the string has been printed
     print()
