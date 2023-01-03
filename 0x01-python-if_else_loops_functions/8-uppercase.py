1e51d2b79ef2c7a62ff43a4406157d256e499779
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2023-01-03 22:58:50 +0300

    Signed-off-by: MarkNWiiam <nkugwamarkwiliam@gmail.com>

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def uppercase(str):
+    # Convert the string to uppercase
     uppercase_str = ""
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            uppercase_str += chr(ord(c) - 32)
+    for ch in str:
+        if ord(ch) >= 97 and ord(ch) <= 122:
+            uppercase_str += chr(ord(ch) - 32)
         else:
-            uppercase_str += c
-    print("{}\n".format(uppercase_str))
+            uppercase_str += ch
+
+    # Print the uppercase string followed by a newline character
+    print(uppercase_str, end="\n")
     
