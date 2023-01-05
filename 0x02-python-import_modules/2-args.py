6a388246c4b3faa72b8290a17dfd48a694ea432f
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2023-01-05 15:50:02 +0300

    Signed-off-by: MarkNWiiam <nkugwamarkwiliam@gmail.com>

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,10 +6,10 @@ def print_arg(argv):
         return
     else:
         if n == 1:
-            print("{:d} argument:".format(n))
+        print("{:d} argument:".format(n))
         else:
-            print("{:d} arguments:".format(n))
-        i = 1
+        print("{:d} arguments:".format(n))
+    i = 1
     while i <= n:
         print("{:d}: {:s}".format(i, argv[i]))
         i += 1
