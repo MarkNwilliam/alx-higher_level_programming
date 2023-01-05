1d49ade42b8bed3d1966b56b8f7ca6efe1c38ee5
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2023-01-05 14:53:40 +0300

    Signed-off-by: MarkNWiiam <nkugwamarkwiliam@gmail.com>

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-    i = 1
-    while i <= n:
-        print("{:d}: {:s}".format(i, argv[i]))
-        i += 1
+import sys
 
-if __name__ == "__main__":
-    import sys
-    print_arg(sys.argv)
+argv = sys.argv[1:] # exclude the name of the script itself
+num_args = len(argv)
+
+if num_args == 0:
+print("0 arguments.")
+elif num_args == 1:
+print("1 argument:")
+else:
+print(f"{num_args} arguments:")
+
+for i, arg in enumerate(argv, start=1):
+print(f"{i}: {arg}")
