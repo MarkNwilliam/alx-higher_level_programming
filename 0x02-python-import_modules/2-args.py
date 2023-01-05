64fa1fed118a93630a0defd4cf9c8ae421d4cc3e
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2023-01-05 15:37:40 +0300

    Signed-off-by: MarkNWiiam <nkugwamarkwiliam@gmail.com>

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ def print_arg(argv):
         return
     else:
         if n == 1:
-            print("{:d} arguments:".format(n))
+            print("{:d} argument:".format(n))
         else:
             print("{:d} arguments:".format(n))
     i = 1
