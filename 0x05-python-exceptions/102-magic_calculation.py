95cbe370b18763f889c20774888dfb05ce8adebb
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2023-01-24 01:19:11 +0300

    Signed-off-by: MarkNWiiam <nkugwamarkwiliam@gmail.com>

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -8,6 +9,6 @@ def magic_calculation(a, b):
             else:
                 result += a ** b / i
         except:
-            result += b + a
+            result = b + a
             break
-    return result
+    return (result)
