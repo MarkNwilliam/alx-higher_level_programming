57667aff92a40239347d9b175835127150d9a8d4
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2023-02-06 21:48:54 +0300

    Signed-off-by: MarkNWiiam <nkugwamarkwiliam@gmail.com>

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 ''' module: 4-inherits_from
 '''
-
-
 def inherits_from(obj, a_class):
     '''the object is an instance of a class that inherited (directly or indirectly)
     obj: an object
