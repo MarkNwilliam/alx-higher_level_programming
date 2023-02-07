fcdd43eaddd21365d4f8ace62e585f479fc63e6e
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2023-02-07 21:39:09 +0300

    Signed-off-by: MarkNWiiam <nkugwamarkwiliam@gmail.com>

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""
-function that writes a string to a text file (UTF8) and returns the number of characters written
-"""
+"""this module writes to a file"""
 
 
 def write_file(filename="", text=""):
-    """ module write_file
-    """
-    with open(filename, 'w') as f:
+    """writes a string to a text file and returns
+    the number of characters written"""
+    with open(filename, encoding="utf-8", mode="w+") as f:
         return f.write(text)
