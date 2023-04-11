56b51e2af44259ede2ac99aab9a47a2ea39f44ea
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2023-04-12 00:49:23 +0300

    Signed-off-by: MarkNWiiam <nkugwamarkwiliam@gmail.com>

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
+// Use strict mode to enforce better coding practices
+'use strict';
+
 const numArgs = process.argv.length - 2;
+
 if (numArgs === 0) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (numArgs === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
