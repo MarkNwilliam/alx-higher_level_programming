dbf3b1bf369659641d2e4e71bb8d3f4c039c6f7f
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2023-04-12 01:41:54 +0300

    Signed-off-by: MarkNWiiam <nkugwamarkwiliam@gmail.com>

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
 
-// Extract command-line arguments and convert to integers
 let args = process.argv.slice(2).map((x) => {
-    return parseInt(x);
+  return parseInt(x);
 });
 
-// If there are less than two arguments, output 0
 if (args.length <= 1) {
-    console.log(0);
+  console.log(0);
 } else {
-    // Sort the arguments in descending order and output the second largest
-    console.log(args.sort((a, b) => {
-	return b - a;
-    })[1]);
+  console.log(args.sort((a, b) => {
+    return b - a;
+  })[1]);
 }
