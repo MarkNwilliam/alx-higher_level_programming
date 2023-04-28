da5b5c363643e418cd56a86a4f0221497ae948cf
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2023-04-28 04:46:42 +0300

    Signed-off-by: MarkNWiiam <nkugwamarkwiliam@gmail.com>

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script takes a URL, sends a request to it, and displays the size of the body of the response in bytes
-
-url=$1
-curl -sI "$url" | grep 'Content-Length' | cut -d' ' -f2
+# Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -s "$1" | wc -c
